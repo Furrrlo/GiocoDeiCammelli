78e1ed4200ddc5ba99665bfd3984f8253640dedf
Frarlo <ferlolovesnewmail@gmail.com>
2019-05-08 21:35:57 +0200

    Style

--- src/main/java/me/palla/entity/EntityManager.java
+++ src/main/java/me/palla/entity/EntityManager.java
@@ -4,28 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EntityManager {
+
     private final List<Entity> entities;
-    
     private final PoolList pools;
  
     public EntityManager(int poolNumberX,int poolNumberY) {
         this.entities = new ArrayList<>();
-        pools=new PoolList(poolNumberX,poolNumberY);
+
         this.addEntity(new BallEntity());
-        for(int i=0;i<pools.getList().size();i++){
+
+        this.pools = new PoolList(poolNumberX,poolNumberY);
+        for(int i = 0; i < pools.getList().size(); i++) {
             this.addEntity(pools.getList().get(i));
         }
     }
 
     public void render() {
-        for(int i=0;i<entities.size();i++){
+        for(int i = 0; i < entities.size(); i++) {
             entities.get(i).onRender();
         }
     }
-    
-    public void addEntity(Entity e){
-        
+
+    private void addEntity(Entity e){
         entities.add(e);
-        
     }
 }
--- src/main/java/me/palla/entity/PoolList.java
+++ src/main/java/me/palla/entity/PoolList.java
@@ -1,8 +1,9 @@
 
 package me.palla.entity;
+import me.palla.GiocoPalla;
+
 import java.util.ArrayList;
 import java.util.List;
-import me.palla.GiocoPalla;
 
 public class PoolList {
     
@@ -15,48 +16,44 @@ public class PoolList {
     private int lastY;
     private int lenghtX;
     private int lenghtY;
-    private final int border=50;
+    private final int border = 50;
     
     
      
     public PoolList(int poolNumberX,int poolNumberY){
-        this.poolNumberX=poolNumberX;
-        this.poolNumberY=poolNumberY;
-        lastX=border;
-        lastY=border;
-        lenghtX=(GiocoPalla.getInstance().getSizeX()-(border*2))/(poolNumberX);
-        lenghtY=(GiocoPalla.getInstance().getSizeY()-(border*2))/(poolNumberY);
+        this.poolNumberX = poolNumberX;
+        this.poolNumberY = poolNumberY;
+        lastX = border;
+        lastY = border;
+        lenghtX = (GiocoPalla.getInstance().getSizeX() - (border * 2)) / poolNumberX;
+        lenghtY = (GiocoPalla.getInstance().getSizeY() - (border * 2)) / poolNumberY;
         this.list = new ArrayList<>();
-        for(int i=0;i<poolNumberX;i++){
-            for(int j=0;j<poolNumberY;j++){         
-                addPool(new PoolEntity(lastX,lastY,lenghtX,lenghtY,10,10,5));
-                lastX+=lenghtX+2;
+        for(int i = 0; i < poolNumberX; i++){
+            for(int j = 0; j < poolNumberY; j++){
+                addPool(new PoolEntity(lastX, lastY, lenghtX, lenghtY,10,10,5));
+                lastX += lenghtX + 2;
             }
-            lastX=border;
-            lastY+=lenghtY+2;
+            lastX = border;
+            lastY += lenghtY + 2;
         }
     }
     
-    public void addPool(PoolEntity pool){
-        if(list.size()<poolNumberX*poolNumberY){
+    private void addPool(PoolEntity pool){
+        if(list.size() < poolNumberX * poolNumberY){
             list.add(pool);
-        } 
+        }
     }
 
     public int getPoolNumberX() {
         return poolNumberX;
     }
-    
-     public int getPoolNumberY() {
+
+    public int getPoolNumberY() {
         return poolNumberY;
     }
 
     public List<PoolEntity> getList() {
         return list;
     }
-    
-    
-    
-    
 }
 
