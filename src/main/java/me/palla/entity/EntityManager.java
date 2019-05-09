0d38e26ebd5b5b49013fee16865c1c9eecb4b315
TiaBroch <>
2019-05-09 11:55:20 +0200

    Commenti BallEntity, Entity e EntityManager

--- src/main/java/me/palla/entity/EntityManager.java
+++ src/main/java/me/palla/entity/EntityManager.java
@@ -3,15 +3,26 @@ package me.palla.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+@author Mattia Broch
+@version 1.0
+@brief classe per la gestione delle Entity
+*/
 public class EntityManager {
-
+    
+    /*
+    @brief la lista di tutte le Entity 
+    */
     private final List<Entity> entities;
-    private final PoolList pools;
+    /*
+    @brief la lista delle vasche 
+    */
 
     public EntityManager(int poolNumberX, int poolNumberY) {
+        final PoolList pools;
         this.entities = new ArrayList<>();
 
-        this.pools = new PoolList(poolNumberX, poolNumberY);
+        pools = new PoolList(poolNumberX, poolNumberY);
         for(int i = 0; i < pools.getList().size(); i++) {
             this.addEntity(pools.getList().get(i));
         }
