a5b9c2cb09bcd655b53c3c4705f0dfcb142ad620
Brozio01 <>
2019-05-09 10:38:21 +0200

    get e set InputGyroscope

--- src/main/java/me/palla/input/InputGyroscope.java
+++ src/main/java/me/palla/input/InputGyroscope.java
@@ -9,4 +9,20 @@ public class InputGyroscope implements InputData {
         this.xAxis = xAxis;
         this.yAxis = yAxis;
     }
+
+    public float getxAxis() {
+        return xAxis;
+    }
+
+    public void setxAxis(float xAxis) {
+        this.xAxis = xAxis;
+    }
+
+    public float getyAxis() {
+        return yAxis;
+    }
+
+    public void setyAxis(float yAxis) {
+        this.yAxis = yAxis;
+    }
 }
